1573eb6b4c5d8f1ad27d569bdbe0b85288c1e0b9
吳宗融 <bc781208@hotmail.com>
2020-06-29 11:09:28 +0800

    初步版型

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>指針時鐘設計</title>
+    <style>
+        body{
+            width: 100vw;
+            height: 100vh;
+            background-color: #111;
+        }
+        .main{
+            display:flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="main">
+        <img src="./img/bg.png" >
+        <?php 
+        include "./img/gear-01.svg";
+        ?>
+    </div>
+    
+</body>
+</html>
